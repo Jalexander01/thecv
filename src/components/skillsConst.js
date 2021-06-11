427fec1b53794e346eddc13dce1387242c020a40
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-06-10 19:01:44 -0500

    refactoring

---
+++ src/components/skillsConst.js
@@ -0,0 +1,25 @@
+const Skills = [
+    {
+        id: 1,
+        title: "Computer Programming Specialist.",
+        img: "Tense Biceps",
+        skill:
+            "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+        id: 2,
+        title: "Web Developer.",
+        img: "Tense Biceps",
+        skill:
+            "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+        id: 3,
+        title: "Chicken Wings Connoisseur.",
+        img: "Tense Biceps",
+        skill:
+            "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    }
+];
+
+export default Skills;
